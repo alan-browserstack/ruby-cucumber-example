ab96e521611d89d9d56e10515aad89fc30adf043
Alan Grubb <alan@browserstack.com>
2020-07-27 09:56:43 -0700

    Clean up env.rb comments

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,18 +16,9 @@ capabilities = {
   "browserVersion" => "latest",
 }
 
-# caps = Selenium::WebDriver::Remote::Capabilities.new
-# caps['os'] = 'Windows'
-# caps['os_version'] = '10'
-# caps["resolution"] = "1920x1080"
-# caps["browser"] = "Chrome"
-# caps["browser_version"] = "latest"
-# caps["build"] = "Ruby Cucumber Example"
-# caps["name"] = "browserstack-inception-test"
 driver = Selenium::WebDriver.for(:remote,
   url: "https://#{ENV['BROWSERSTACK_USERNAME']}:#{ENV['BROWSERSTACK_KEY']}@hub-cloud.browserstack.com/wd/hub",
   desired_capabilities: capabilities)
-# driver = Selenium::WebDriver.for :chrome
 
 Before do |scenario|
   @driver = driver
